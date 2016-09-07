853f467a2368061f8cec7e57147ff9e2e90b0e97
ingelectronicadj <ingelectronicadj@gmail.com>
2016-09-07 12:09:29 -0500

    Se reduce a un solo pin el tema de generar una señal que consuma el numero aleatorio generado por la semilla de 3 bits

--- numerosAleatorios/numerosAleatorios.ino
+++ numerosAleatorios/numerosAleatorios.ino
@@ -3,10 +3,8 @@
  *              LIBRERIAS y CONSTANTES
  ***********************************************************************
  */
-int IN1 = 8; 
-int IN2 = 7;
-int IN3 = 6;
-int tiempoDePulso = 500; // 0.5s
+int senal = 8; 
+int ts = 100; // 0.5s
  
 int azar;
 
@@ -17,9 +15,7 @@ int azar;
  */
 void setup(){
   Serial.begin(9600);    // Tasa de baudios del puerto serial
-  pinMode (IN1, OUTPUT); // Salidas de pulso
-  pinMode (IN2, OUTPUT);
-  pinMode (IN3, OUTPUT);
+  pinMode (senal, OUTPUT); // Salidas de pulso
   
 }
 
@@ -32,52 +28,68 @@ void loop(){
   numeroAletorio();
   //delay(300);
   if(azar == 0){ // Si el numero aleatorio es 0 = 000
-      digitalWrite(IN1,LOW);
-      digitalWrite(IN2,LOW);
-      digitalWrite(IN3,LOW);
-      delay(tiempoDePulso);
+      digitalWrite(senal,LOW);
+      delay(ts);
+      digitalWrite(senal,LOW);
+      delay(ts);
+      digitalWrite(senal,LOW);
+      delay(ts);
     }
   if(azar == 1){ // Si el numero aleatorio es 1 = 001
-      digitalWrite(IN1,LOW);
-      digitalWrite(IN2,LOW);
-      digitalWrite(IN3,HIGH);
-      delay(tiempoDePulso);
+      digitalWrite(senal,LOW);
+      delay(ts);
+      digitalWrite(senal,LOW);
+      delay(ts);
+      digitalWrite(senal,HIGH);
+      delay(ts);
     }
   if(azar == 2){ // Si el numero aleatorio es 2 = 010
-      digitalWrite(IN1,LOW);
-      digitalWrite(IN2,HIGH);
-      digitalWrite(IN3,LOW);
-      delay(tiempoDePulso);
+      digitalWrite(senal,LOW);
+      delay(ts);
+      digitalWrite(senal,HIGH);
+      delay(ts);
+      digitalWrite(senal,LOW);
+      delay(ts);
     }
   if(azar == 3){ // Si el numero aleatorio es 3 = 011
-      digitalWrite(IN1,LOW);
-      digitalWrite(IN2,HIGH);
-      digitalWrite(IN3,HIGH);
-      delay(tiempoDePulso);
+      digitalWrite(senal,LOW);
+      delay(ts);
+      digitalWrite(senal,HIGH);
+      delay(ts);
+      digitalWrite(senal,HIGH);
+      delay(ts);
     }
   if(azar == 4){ // Si el numero aleatorio es 4 = 100
-      digitalWrite(IN1,HIGH);
-      digitalWrite(IN2,LOW);
-      digitalWrite(IN3,LOW);
-      delay(tiempoDePulso);
+      digitalWrite(senal,HIGH);
+      delay(ts);
+      digitalWrite(senal,LOW);
+      delay(ts);
+      digitalWrite(senal,LOW);
+      delay(ts);
     }
   if(azar == 5){ // Si el numero aleatorio es 6 = 101
-      digitalWrite(IN1,HIGH);
-      digitalWrite(IN2,LOW);
-      digitalWrite(IN3,HIGH);
-      delay(tiempoDePulso);
+      digitalWrite(senal,HIGH);
+      delay(ts);
+      digitalWrite(senal,LOW);
+      delay(ts);
+      digitalWrite(senal,HIGH);
+      delay(ts);
     }
   if(azar == 6){ // Si el numero aleatorio es 6 = 110
-      digitalWrite(IN1,HIGH);
-      digitalWrite(IN2,HIGH);
-      digitalWrite(IN3,LOW);
-      delay(tiempoDePulso);
+      digitalWrite(senal,HIGH);
+      delay(ts);
+      digitalWrite(senal,HIGH);
+      delay(ts);
+      digitalWrite(senal,LOW);
+      delay(ts);
     }
   if(azar == 7){ // Si el numero aleatorio es 7 = 111
-      digitalWrite(IN1,HIGH);
-      digitalWrite(IN2,HIGH);
-      digitalWrite(IN3,HIGH);
-      delay(tiempoDePulso);
+      digitalWrite(senal,HIGH);
+      delay(ts);
+      digitalWrite(senal,HIGH);
+      delay(ts);
+      digitalWrite(senal,HIGH);
+      delay(ts);
     }
   }
    
