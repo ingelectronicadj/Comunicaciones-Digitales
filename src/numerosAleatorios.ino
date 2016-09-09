dfdb539011b81b5b2e519dbba443762aca7e04b5
ingelectronicadj <ingelectronicadj@gmail.com>
2016-09-08 23:39:52 -0500

    Se ejecuta el sketch con platform.io del editor de texto ATOM, se crea funcion para generar la señal de pulsos aleatoria y se añaden variables para los ejeX y ejeY que representaran niveles de tension en cuadratura

--- src/numerosAleatorios.ino
+++ src/numerosAleatorios.ino
@@ -3,10 +3,11 @@
  *              LIBRERIAS y CONSTANTES
  ***********************************************************************
  */
-int senal = 8; 
-int ts = 100; // 0.5s
- 
 int azar;
+int senal = 8;
+int ts = 200; // millis
+int ejeX = 6;
+int ejeY= 7;
 
 /*
  ***********************************************************************
@@ -16,7 +17,8 @@ int azar;
 void setup(){
   Serial.begin(9600);    // Tasa de baudios del puerto serial
   pinMode (senal, OUTPUT); // Salidas de pulso
-  
+  pinMode (ejeX, OUTPUT); // Nivel de tension sobre el eje X
+  pinMode (ejeY, OUTPUT); // Nivel de tension sobre el eje Y
 }
 
  /*
@@ -26,7 +28,22 @@ void setup(){
  */
 void loop(){
   numeroAletorio();
-  //delay(300);
+  generaSenal();
+  tensionCuadratura();
+  delay(ts);
+  }
+
+
+void numeroAletorio(){
+    /*Si cargamos un #constante como semilla generamos
+    numeros seudo-aeatorios, cargando una semilla cambiante
+    generamos numeros aleatorios*/
+    randomSeed(millis());
+    azar = random(0,8); // genera numero aleatorio desde random(n,(m-1))
+    Serial.println(azar);
+  }
+
+void generaSenal(){
   if(azar == 0){ // Si el numero aleatorio es 0 = 000
       digitalWrite(senal,LOW);
       delay(ts);
@@ -82,8 +99,8 @@ void loop(){
       delay(ts);
       digitalWrite(senal,LOW);
       delay(ts);
-    }
-  if(azar == 7){ // Si el numero aleatorio es 7 = 111
+    }else{ // Si el numero aleatorio es 7 = 111
+      //(azar == 7){
       digitalWrite(senal,HIGH);
       delay(ts);
       digitalWrite(senal,HIGH);
@@ -91,16 +108,11 @@ void loop(){
       digitalWrite(senal,HIGH);
       delay(ts);
     }
-  }
-   
-  
-void numeroAletorio(){
-    /*Si cargamos un #constante como semilla generamos 
-    numeros seudo-aeatorios, cargando una semilla cambiante
-    generamos numeros aleatorios*/
-    randomSeed(millis());  
-    azar = random(0,8); // genera numero aleatorio desde random(n,(m-1))
-    Serial.println(azar);
-  }
+}
 
-
+void tensionCuadratura(){
+  if(azar <= 3){
+    digitalWrite(ejeX,30);
+    digitalWrite(ejeY,30);
+  }
+}
