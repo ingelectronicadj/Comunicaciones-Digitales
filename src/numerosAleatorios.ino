ad31b0390fb5e1c0f3ec765222de4d46fb92ac00
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-10-11 21:22:28 -0500

    Se configura mapeo de entrada analógica -5 a 5v con CLK y se muestra la tupla en puerto serial

--- src/numerosAleatorios.ino
+++ src/numerosAleatorios.ino
@@ -8,15 +8,21 @@
 
 const byte pinOutSenalDigital = 13; // Pin de Salida de Senal
 const byte pinOutClk1 = 12;         // pin clock triada
-const int ts = 25;                 // tiempo en millis
-const long deltaT = 22500;         // microsegundos 180000us 180ms
+const int ts = 25;                  // tiempo en millis
+const long deltaT = 22500;          // microsegundos 180000us 180ms
 // Constantes tipo byte va de 0 a 255, es suficiente para representar los pines
 
 const byte ledPin = 53;           // Indicador de interrupcion, led de salida
 const byte pinInClk1 = 2;         // Pin de entrada de la interrupcion
 const byte pinInSenalDigital = 3; // Pin de entrada de la senal digital
 
+const byte pinOutClk2 = 46;         // Pin de salida del CLK2
+const byte pinInClk2 = 18;          // Pin de entrada de la interrupcion
+const byte pinInSenalAnalogaX = A8; // Pin de entrada de la senal analoga
+const byte pinInSenalAnalogaY = A9; // Pin de entrada de la senal analoga
+
 bool estadoClk1 = LOW; // Estado inicial de clk1
+bool estadoClk2 = LOW; // Estado inicial de clk1
 
 const byte pinX = 11;
 const byte pinY = 10;
@@ -41,19 +47,24 @@ void setup() {
   pinMode(pinOutSenalDigital, OUTPUT);      // Señal de pulsos aleatorios
   pinMode(pinInClk1, INPUT_PULLUP); // Pin como entrada de la interrupcion
   pinMode(pinOutClk1, OUTPUT);      // Señal clk1
+  pinMode(pinInClk2, INPUT_PULLUP); // Pin como entrada de la interrupcion
+  pinMode(pinOutClk2, OUTPUT);      // Señal clk1
   pinMode(pinX, OUTPUT);            // Salida de tension sobre el eje X
   pinMode(pinY, OUTPUT);            // "               " sobre el eje Y
   pinMode(pinMenosX, OUTPUT);       // Salida de tension sobre el eje menosX
   pinMode(pinMenosY, OUTPUT);       // "               " sobre el eje menosY
   pinMode(pinXmedios, OUTPUT);      // Salida de tension sobre el eje X medios
   pinMode(pinYmedios, OUTPUT);      // "               " sobre el eje Y medios
-  pinMode(pinMenosXmedios, OUTPUT); // Salida de tension sobre el eje menosXmedios
-  pinMode(pinMenosYmedios, OUTPUT); // "               " sobre el eje menosYmedios
+  pinMode(pinMenosXmedios,
+          OUTPUT); // Salida de tension sobre el eje menosXmedios
+  pinMode(pinMenosYmedios,
+          OUTPUT); // "               " sobre el eje menosYmedios
   pinMode(ledPin, OUTPUT);
   Timer1.initialize(deltaT); // 180 ms
   Timer1.attachInterrupt(ISR_Callback);
   Timer1.stop();
   attachInterrupt(digitalPinToInterrupt(pinInClk1), clk1Detection, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(pinInClk2), clk2Detection, CHANGE);
 }
 
 /*
@@ -160,41 +171,43 @@ void codificar() {
   byte valor;
   if (buffer[0] == LOW && buffer[1] == LOW && buffer[2] == LOW) {
     valor = 0;
-    //pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
+    // pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
     escribirSenalAnaloga(LOW, LOW, LOW, LOW, LOW, LOW, LOW, HIGH);
   } else if (buffer[0] == LOW && buffer[1] == LOW && buffer[2] == HIGH) {
     valor = 1;
-    //pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
+    // pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
     escribirSenalAnaloga(LOW, LOW, LOW, LOW, HIGH, LOW, LOW, LOW);
   } else if (buffer[0] == LOW && buffer[1] == HIGH && buffer[2] == LOW) {
     valor = 2;
-    //pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
+    // pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
     escribirSenalAnaloga(LOW, LOW, LOW, LOW, LOW, HIGH, LOW, LOW);
   } else if (buffer[0] == LOW && buffer[1] == HIGH && buffer[2] == HIGH) {
     valor = 3;
-    //pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
+    // pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
     escribirSenalAnaloga(LOW, LOW, LOW, LOW, LOW, LOW, HIGH, LOW);
   } else if (buffer[0] == HIGH && buffer[1] == LOW && buffer[2] == LOW) {
     valor = 4;
-    //pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
+    // pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
     escribirSenalAnaloga(HIGH, HIGH, LOW, LOW, LOW, LOW, LOW, LOW);
   } else if (buffer[0] == HIGH && buffer[1] == LOW && buffer[2] == HIGH) {
     valor = 5;
-    //pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
+    // pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
     escribirSenalAnaloga(LOW, HIGH, HIGH, LOW, LOW, LOW, LOW, LOW);
   } else if (buffer[0] == HIGH && buffer[1] == HIGH && buffer[2] == LOW) {
     valor = 6;
-    //pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
+    // pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
     escribirSenalAnaloga(LOW, LOW, HIGH, HIGH, LOW, LOW, LOW, LOW);
   } else { // if (buffer[0] == HIGH && buffer[1] == HIGH && buffer[2] == HIGH)
     valor = 7;
-    //pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
+    // pinX,pinY,pinMenosX,pinMenosY,pinXmedios,pinYmedios,pinMenosXmedios,pinMenosYmedios
     escribirSenalAnaloga(HIGH, LOW, LOW, HIGH, LOW, LOW, LOW, LOW);
   }
   Serial.println("Valor: " + String(valor) + "\n\n");
 }
 
-void escribirSenalAnaloga(byte ejeX, byte ejeY, byte ejeMenosX, byte ejeMenosY, byte ejeXmedios, byte ejeYmedios, byte ejeMenosXmedios, byte ejeMenosYmedios) {
+void escribirSenalAnaloga(bool ejeX, bool ejeY, bool ejeMenosX, bool ejeMenosY,
+                          bool ejeXmedios, bool ejeYmedios,
+                          bool ejeMenosXmedios, bool ejeMenosYmedios) {
   digitalWrite(pinX, ejeX);
   digitalWrite(pinY, ejeY);
   digitalWrite(pinMenosX, ejeMenosX);
@@ -203,5 +216,54 @@ void escribirSenalAnaloga(byte ejeX, byte ejeY, byte ejeMenosX, byte ejeMenosY, 
   digitalWrite(pinYmedios, ejeYmedios);
   digitalWrite(pinMenosXmedios, ejeMenosXmedios);
   digitalWrite(pinMenosYmedios, ejeMenosYmedios);
-  //delay(5);
+  cambiarYEnviarClk2();
+}
+
+void cambiarYEnviarClk2() {
+  digitalWrite(pinOutClk2, estadoClk2);
+  estadoClk2 = !estadoClk2;
+}
+
+void clk2Detection() {
+  int x = analogRead(pinInSenalAnalogaX);
+  float voltageX = x * (5.0 / 1023.0);
+  byte posX = voltajeAPosicion(voltageX);
+
+  int y = analogRead(pinInSenalAnalogaY);
+  float voltageY = y * (5.0 / 1023.0);
+  byte posY = voltajeAPosicion(voltageY);
+
+  if (posX == 2 && posY == 1) { // 000
+    Serial.println('Posicion: 000');
+  } else if (posX == 3 && posY == 2) { // 001
+    Serial.println('Posicion: 001');
+  } else if (posX == 2 && posY == 3) { // 010
+    Serial.println('Posicion: 010');
+  } else if (posX == 1 && posY == 2) { // 011
+    Serial.println('Posicion: 011');
+  } else if (posX == 4 && posY == 4) { // 100
+    Serial.println('Posicion: 100');
+  } else if (posX == 0 && posY == 4) { // 101
+    Serial.println('Posicion: 101');
+  } else if (posX == 0 && posY == 0) { // 110
+    Serial.println('Posicion: 110');
+  } else { // (posX == 4 && posY == 0) // 111
+    Serial.println('Posicion: 111');
+  }
+}
+
+byte voltajeAPosicion(float voltaje) {
+  byte posicion = 0;
+  if (voltaje >= 0 && voltaje < 0.625) {
+    posicion = 0;
+  } else if (voltaje >= 0.625 && voltaje < 1.875) {
+    posicion = 1;
+  } else if (voltaje >= 1.875 && voltaje < 3.125) {
+    posicion = 2;
+  } else if (voltaje >= 3.125 && voltaje < 4.375) {
+    posicion = 3;
+  } else { // voltaje >= 4.375 && voltaje < 5
+    posicion = 4;
+  }
+  return posicion;
 }
